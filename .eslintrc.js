c54cf0725d0c6d1a96a97a0a4332d01211d0eff1
Rishabh Singh <rrishabhssingh@gmail.com>
2025-06-21 01:02:28 +0530

    Disable ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,22 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      ecmaFeatures: { jsx: true },
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-    },
-    plugins: ["@typescript-eslint", "react"],
-    extends: [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:@typescript-eslint/recommended",
-    ],
-    rules: {
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-      "react/react-in-jsx-scope": "off", // if using React 17+ JSX transform
-    },
-  };
-  
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: [],
+  extends: [],
+  rules: {},
+};
