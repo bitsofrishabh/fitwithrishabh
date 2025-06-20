f15d0468b614bd831566aa7d046e15b1f235bad7
Rishabh Singh <rrishabhssingh@gmail.com>
2025-06-21 02:08:55 +0530

    chore: centralize eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,37 @@
+import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+
 export default [
   {
+    ignores: ['public/**'],
+  },
+  {
     files: ['**/*.{js,jsx,ts,tsx}'],
-    rules: {},
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+    },
   },
 ];
